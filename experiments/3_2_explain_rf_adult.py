bd2a4c8049edb2db7df776cc864fb84e3c12ccb5
galabc <gabriel.laberge@polymtl.ca>
2022-11-30 19:16:41 -0600

    Run TreeSHAP on Adult RFs.

---
+++ experiments/3_2_explain_rf_adult.py
@@ -0,0 +1,48 @@
+import numpy as np
+from joblib import load, dump
+import matplotlib.pyplot as plt
+from simple_parsing import ArgumentParser
+
+# Local imports
+from utils import custom_train_test_split, setup_pyplot_font, setup_data_trees
+
+import sys, os
+sys.path.append(os.path.join('../'))
+from uxai.trees import all_tree_preds, epsilon_upper_bound, tree_attributions
+
+
+if __name__ == "__main__":
+
+    setup_pyplot_font(20)
+
+    parser = ArgumentParser()
+    parser.add_argument("--seed", type=int, default=0, help="Random Seed")
+    parser.add_argument("--background_size", type=int, default=100, help="Number of background samples")
+    args, unknown = parser.parse_known_args()
+    print(args)
+
+    X, y, features, task, ohe = setup_data_trees("adult_income")
+    X = ohe.transform(X)
+    X_train, X_test, y_train, y_test = custom_train_test_split(X, y, task)
+    M = 1000
+
+    model = load(os.path.join("models", "Adult-Income", f"RF_M_{M}_seed_{args.seed}.joblib"))
+
+    # The upper bound on preformance
+    tree_preds = all_tree_preds(X_test, model, task="classification")
+    m, epsilon_upper = epsilon_upper_bound(tree_preds, y_test.reshape((-1, 1)), task="classification")
+
+    # Shap feature attribution
+    foreground = X_test[:2000]
+    background = X_train[:args.background_size]
+    gaps = model.predict_proba(foreground)[:, 1] - model.predict_proba(X_train)[:, 1].mean()
+    rashomon_po = tree_attributions(model, foreground, background, epsilon_upper, features, ohe)
+
+    # Record the Gap Error
+    gap_errors = rashomon_po.phi_mean.sum(1) - gaps
+    tmp_filename = f"GapErrors_M_{M}_seed_{args.seed}_background_{args.background_size}.joblib"
+    np.save(gap_errors, os.path.join("models", "Adult-Income", tmp_filename) )
+
+    # Save the Rashomon Partial Order Object
+    tmp_filename = f"TreeSHAP_M_{M}_seed_{args.seed}_background_{args.background_size}.joblib"
+    dump(rashomon_po, os.path.join("models", "Adult-Income", tmp_filename) )
